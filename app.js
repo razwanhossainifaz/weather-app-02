ac2cdc09294bb9618fca1334de78e74039fae9ca
freelancerifaz <mrifaz369@gmail.com>
2020-08-16 00:33:30 +0600

    weather app 02 done

--- app.js
+++ app.js
@@ -14,3 +14,17 @@ document.getElementById("search-btn").addEventListener('click' , () => {
 
 // function for get api data from openweathermap.org
 
+function getApiData(cityName){
+    fetch(`${weatherApi.baseUrl}?q=${cityName}&appid=${weatherApi.key}&units=metric`)
+    .then(response => response.json())
+    .then(data => showData(data))
+    return cityName;
+};
+
+// show city name , temperature in degree , weather type and image change
+
+function showData(weather){
+    document.getElementById("city-name").innerText = weather.name + " " + weather.sys.country;
+    document.getElementById("temp").innerText = weather.main.temp;
+    document.getElementById("weather-type").innerText = weather.weather[0].main;
+}
